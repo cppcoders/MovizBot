c051949008b28effc2b3478b6893cd9e61ee982a
sudofix <mo7amed.ram@gmail.com>
2021-05-29 06:05:18 +0200

    make it better

--- app/cht.py
+++ app/cht.py
@@ -18,63 +18,23 @@ app = Flask(__name__)
 
 
 # ---------------------------------------TEl
+import os
 
-global tbot
-global bot_token 
-bot_token = "1805801633:AAGf_VfOwnP6WP61EwYTSTDlMw4_pcnzLQs"
-bot_user_name = "movizsbot"
-URL = "https://movizbot-server.herokuapp.com/"
+TOKEN = os.environ['TOKEN']
 
 
-tbot = telegram.Bot(token=bot_token)
 
 
-@app.route('/{}'.format(bot_token), methods=['POST'])
+tbot = telegram.Bot(token=TOKEN)
+
+@app.route('/{}'.format(TOKEN), methods=['POST'])
 def respond():
-    # retrieve the message in JSON and then transform it to Telegram object
-    update = telegram.Update.de_json(request.get_json(force=True), tbot)
-
+    update = telegram.Update.de_json(request.get_json(force=True), bot)
     chat_id = update.message.chat.id
     msg_id = update.message.message_id
-
-    # Telegram understands UTF-8, so encode text for unicode compatibility
-    text = update.message.text.encode('utf-8').decode()
-    # for debugging purposes only
-    print("got text message :", text)
-    # the first time you chat with the bot AKA the welcoming message
-    if text == "/start":
-        # print the welcoming message
-        bot_welcome = """
-       Welcome to MovizBot
-       """
-        # send the welcoming message
-        tbot.sendMessage(chat_id=chat_id, text=bot_welcome,
-                         reply_to_message_id=msg_id)
-
-    else:
-        try:
-            # clear the message we got from any non alphabets
-            # create the api link for the avatar based on http://avatars.adorable.io/
-            rep = wittel(text)
-            # reply with a photo to the name the user sent,
-            # note that you can send photos by url and telegram will fetch it for you
-            tbot.sendMessage(chat_id=chat_id, text=rep,
-                             reply_to_message_id=msg_id)
-        except Exception:
-            # if things went wrong
-            tbot.sendMessage(
-                chat_id=chat_id, text="There was a problem in the name you used, please enter different name", reply_to_message_id=msg_id)
-
+    msg = update.message.text.encode('utf-8').decode()
+    bot.sendMessage(chat_id=chat_id, text=wittel(msg), reply_to_message_id=msg_id)
     return 'ok'
-
-
-@app.route('/set_webhook', methods=['GET', 'POST'])
-def set_webhook():
-    s = tbot.setWebhook('{URL}{HOOK}'.format(URL=URL, HOOK=bot_token))
-    if s:
-        return "webhook setup ok"
-    else:
-        return "webhook setup failed"
 
 
 def wittel(text):
@@ -140,13 +100,13 @@ def get_wit(sender_id, msg):
         message = mmodule.main_function(intent, entity)
     else:
         message = "I'm not sure what to do"
-        
+    '''    
     reply(sender_id, message)
-    '''
+    
 
 def reply(sender_id, response):
     bot.send_text_message(sender_id, response)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(threaded=True)
